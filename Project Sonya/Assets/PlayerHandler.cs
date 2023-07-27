0d2823012fc0cbc069da3fe54560355f182280a1
Özgün DOĞAN <ozgundogan2000@gmail.com>
2023-07-27 21:19:16 +0300

    Throw Player

---
+++ Project Sonya/Assets/PlayerHandler.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerHandler : MonoBehaviour
+{
+    [SerializeField] Rigidbody currentPlayerRigidbody;
+    [SerializeField] SpringJoint currentPlayerSpringJoint;
+    [SerializeField] float detachDelay;
+    
+    private Camera mainCamera;
+    private float cameraZDistance;
+    private Vector3 screenPosition;
+    private bool isDragging;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        cameraZDistance = mainCamera.WorldToScreenPoint(transform.position).z;
+    }
+
+    void Update()
+    {
+        if (currentPlayerRigidbody == null) { return; }
+        
+        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            if (isDragging)
+            {
+                currentPlayerRigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+                ThrowPlayer();
+            }
+
+            isDragging = false;
+
+            return;
+        }
+
+        isDragging = true;
+        currentPlayerRigidbody.isKinematic = true;
+
+        Vector2 touchPos = Touchscreen.current.primaryTouch.position.ReadValue();
+        screenPosition = new Vector3(touchPos.x, touchPos.y, cameraZDistance);
+
+        Vector3 worldPos = mainCamera.ScreenToWorldPoint(screenPosition);
+        currentPlayerRigidbody.position = worldPos;
+    }
+
+    private void ThrowPlayer()
+    {
+        currentPlayerRigidbody.isKinematic = false;
+        currentPlayerRigidbody = null;
+
+        Invoke(nameof(DetachBall), detachDelay);
+    }
+
+    private void DetachBall()
+    {
+        currentPlayerSpringJoint.gameObject.SetActive(false);
+    }
+}
